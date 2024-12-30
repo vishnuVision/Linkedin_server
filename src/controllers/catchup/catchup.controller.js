b4ac1fbe06fcf7419dc380efd9e989cb5dd80d63
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-12-30 16:57:41 +0530

    updating catup controller

--- src/controllers/catchup/catchup.controller.js
+++ src/controllers/catchup/catchup.controller.js
@@ -5,23 +5,31 @@ import { sendResponse } from "../../utils/SendResponse.js";
 
 const getAllCatchup = async (req, res, next) => {
     try {
-        if(!req.user)
+        if (!req.user)
             return next(new ErrorHandler("Please login", 400));
 
         const user = await User.findById(req.user.id);
 
-        if(!user)    
+        if (!user)
             return next(new ErrorHandler("User not found", 400));
 
-        const catchup = await Catchup.find({owner: {$in:[...user?.following,...user?.followers]}}).populate("owner","firstName lastName avatar").populate({path:"referenceId",select:"company",populate:{path:"company",model:"Page",select:"name"}});
+        const catchup = await Catchup.find({
+            owner: { $in: [...(user?.following || []), ...(user?.followers || [])] }
+        })
+            .populate("owner", "firstName lastName avatar")
+            .populate({
+                path: "referenceId",
+                select: "company",
+                populate: { path: "company", model: "Page", select: "name" }
+            });
 
-        if(!catchup)
+        if (!catchup)
             return next(new ErrorHandler("Catchup not found", 400));
 
-        return sendResponse(res, 200, "Catchup fetched Successfully!", true, catchup, null);        
+        return sendResponse(res, 200, "Catchup fetched Successfully!", true, catchup, null);
     } catch (error) {
         return next(new ErrorHandler(error.message, 500));
-    }   
+    }
 }
 
 export {
