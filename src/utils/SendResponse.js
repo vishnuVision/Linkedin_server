ec49d86fd87c6f88b58ecba5c4a0d0174ebc364e
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-12-31 09:32:46 +0530

    cookie improvemnt

--- src/utils/SendResponse.js
+++ src/utils/SendResponse.js
@@ -1,9 +1,8 @@
 const cookieOption = {
     maxAge: 24 * 60 * 60 * 1000,
-    // httpOnly: true,
-    // sameSite: "none",
-    // secure: true,
-    // path: "/"
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
 }
 
 const sendResponse = (res, status, message, success, data, cookie) => {
