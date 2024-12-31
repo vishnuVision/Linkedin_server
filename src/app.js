362ca5280f77a848b4092ca2597c9492d6f5ac5b
Vishnu Mandlesara <140898898+Vishnu-0609@users.noreply.github.com>
2024-12-31 19:01:19 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173","https://client-linked.netlify.app/"],
+        origin: ["http://localhost:5173","https://client-linked.netlify.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
